233fa5021ba54a55b05606696c600669248c40a8
derkaiser28 <crisgrace@Criss-MacBook-Pro.local>
2024-09-12 12:02:23 +0530

    again

--- src/components/category.vue
+++ src/components/category.vue
@@ -4,21 +4,21 @@
       <h2 class="text-2xl font-bold tracking-tight text-gray-900">{{ title }}</h2>
 
       <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-        <div v-for="product in products" :key="product.id" class="group relative">
+        <div v-for="movie in movies" :key="movie.id" class="group relative">
           <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-            <img :src="product.imageSrc" :alt="product.imageAlt" class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
+            <img :src="movie.imageSrc" :alt="movie.imageAlt" class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
           </div>
           <div class="mt-4 flex justify-between">
             <div>
               <h3 class="text-sm text-gray-700">
-                <a :href="product.href">
+                <a :href="movie.href">
                   <span aria-hidden="true" class="absolute inset-0" />
-                  {{ product.name }}
+                  {{ movie.name }}
                 </a>
               </h3>
-              <p class="mt-1 text-sm text-gray-500">{{ product.color }}</p>
+              <p class="mt-1 text-sm text-gray-500">{{ movie.color }}</p>
             </div>
-            <p class="text-sm font-medium text-gray-900">{{ product.price }}</p>
+            <p class="text-sm font-medium text-gray-900">{{ movie.price }}</p>
           </div>
         </div>
       </div>
@@ -30,7 +30,6 @@
     import { defineProps } from 'vue'
 
     defineProps({
-        title: String,
-        movies: Object
+        movie: Object
     })
 </script>
--- src/views/categories.vue
+++ src/views/categories.vue
@@ -1,15 +1,37 @@
 <template>
         <div class="m-10">
             <Tabs></Tabs>
-            
+                <category class="my-5"></category>
+                <category class="my-5"></category>
+                <category class="my-5"></category>
+                <category class="my-5"></category>
+                <category class="my-5"></category>
         </div>
 </template>
 
 <script setup>
     import Tabs from '../components/tabs.vue'
+    import category from '../components/category.vue'
     import { appStore } from '../pinia/appStore.js';
     import Button from 'primevue/Button'
+    import { ref } from 'vue'
 
     const appPinia = appStore();
+
+    const categoryData = ref()
+
+    const data = {
+        langaues: [
+
+        ],
+        genres: [
+
+        ],
+        sports: [
+
+        ]
+    }
+
+
     
 </script>
